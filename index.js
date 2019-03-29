06604c0d778c0c14c1f56be93d7ad4217ccf86e6
Callan Morrison <callan.l.morrison@gmail.com>
2019-03-29 18:57:59 -0500

    port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const moment = require('moment');
 const cors = require('cors')
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express(); 
 
 app.use(cors())
